3052dc8df04d03a177d7910af26a805d514ac10f
lchudinov <lchudinov@users.noreply.github.com>
2022-06-30 13:41:45 +0000

    deploy: dc5409b26088c5c3bc9bf8ff86c694cdce3afa00

--- src/app/api/services.ts
+++
@@ -1,9 +0,0 @@
-export { CargoService } from './services/cargo.service';
-export { CompanyService } from './services/company.service';
-export { ContractorService } from './services/contractor.service';
-export { CustomerService } from './services/customer.service';
-export { DirectionService } from './services/direction.service';
-export { RequestService } from './services/request.service';
-export { SettingsService } from './services/settings.service';
-export { TransportService } from './services/transport.service';
-export { UserService } from './services/user.service';
