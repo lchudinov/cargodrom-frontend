86a8e10e09cb2f2afc3950e37d3e4822ad45d5e0
Vladimir <vlak66@gmail.com>
2022-07-15 17:13:41 +0500

    Фиксим тесты

--- src/app/auth/components/register/register.component.spec.ts
+++ src/app/auth/components/register/register.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MaterialModule} from "../../../material/material.module";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,6 +12,12 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MaterialModule
+      ],
       declarations: [ RegisterComponent ]
     })
     .compileComponents();
