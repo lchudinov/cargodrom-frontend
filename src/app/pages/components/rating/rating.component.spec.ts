23b41bbd9141fd26fd3524efbf20463f8cc4fdac
Leonty Chudinov <leonty.chudinov@gmail.com>
2022-07-15 21:27:39 +0500

    Add tests for rating component

--- src/app/pages/components/rating/rating.component.spec.ts
+++ src/app/pages/components/rating/rating.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RatingComponent } from './rating.component';
@@ -13,9 +13,9 @@ describe('RatingComponent', () => {
         FormsModule,
         ReactiveFormsModule,
       ],
-      declarations: [ RatingComponent ]
+      declarations: [RatingComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RatingComponent);
     component = fixture.componentInstance;
@@ -25,4 +25,27 @@ describe('RatingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show 4/10 score', () => {
+    const score = 4;
+    component.setScore(score);
+    fixture.detectChanges();
+    const ratingElement: HTMLElement = fixture.nativeElement;
+    expect(ratingElement.textContent).toContain(`${score}/10`);
+    expect(ratingElement.querySelectorAll('.sel').length).toEqual(score);
+  });
+
+  it('should show update score when 3th star clicked', fakeAsync(() => {
+    const starNumberFromOne = 3;
+    const starNumberFromZero = starNumberFromOne - 1;
+    const expectedScore = starNumberFromOne;
+    const ratingElement: HTMLElement = fixture.nativeElement;
+    const stars = ratingElement.querySelectorAll('span.stars>span');
+    const star = stars[starNumberFromZero] as HTMLElement;
+    star.click();
+    tick();
+    fixture.detectChanges();
+    expect(component.score).toEqual(expectedScore);
+  }));
+
 });
